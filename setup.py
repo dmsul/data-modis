9f482f48e227de1cab1ae0d8125222c7b2379009
Daniel Sullivan <sullydm@gmail.com>
2018-02-23 11:04:40 -0500

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='modis',
+      version='0.0.1',
+      description='Easy access to MODIS data.',
+      url='http://github.com/dmsul/modis',
+      packages=['modis']
+      )
