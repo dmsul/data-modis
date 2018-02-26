3efacda6e03011f70e8b75c0497812b072711b4b
MarkNepf <marknepf@gmail.com>
2018-02-26 16:00:43 -0500

    minor edits to run on mark's computer

--- modis/util/env.py
+++ modis/util/env.py
@@ -1,7 +1,18 @@
 import os
+import socket
 
 
-DATA_PATH = 'd:\\Data\\modis'
+# Check which machine we're on
+HOST = socket.gethostname()
+if HOST == 'sullivan-7d':
+    data_root = "D:\\"
+elif HOST == 'DESKTOP-HOME':
+    data_root = "D:\\"
+else:
+    data_root = r'\\Sullivan-7d\d'
+
+
+DATA_PATH = os.path.join(data_root, 'Data', 'modis')
 
 HDF_SRC_PATH_WIN = 'e:\\modis\\src'
 HDF_SRC_PATH_NIX = '/media/sf_modis/src'
