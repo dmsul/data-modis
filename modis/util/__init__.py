886efdb7410182d6d365169cecbf5fe94578e323
MarkNepf <marknepf@gmail.com>
2018-03-09 14:01:48 -0500

    mean_modis to aod

--- modis/util/__init__.py
+++ modis/util/__init__.py
@@ -2,9 +2,9 @@ from clean.raw import load_modis_year
 
 
 def annual_mean(year, spatial_index=None):
-    df = load_modis_year(year)
+    df = load_modis_year(2000)
     quarter = (df['time'].dt.month - 1) // 3 + 1
-    quarter_mean = df.groupby(quarter)['mean_modis'].mean()
+    quarter_mean = df.groupby(quarter)['aod'].mean()
 
 #    if spatial_index is not None:
 #       annual_mean = quarter_mean.groupby(spatial_index).mean()
