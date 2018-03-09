cbe709f09af56f95d051de96e5de1f1a78ebb255
MarkNepf <marknepf@gmail.com>
2018-03-09 13:56:04 -0500

    quarterly and annual average

---
+++ modis/util/__init__.py
@@ -0,0 +1,18 @@
+from clean.raw import load_modis_year
+
+
+def annual_mean(year, spatial_index=None):
+    df = load_modis_year(year)
+    quarter = (df['time'].dt.month - 1) // 3 + 1
+    quarter_mean = df.groupby(quarter)['mean_modis'].mean()
+
+#    if spatial_index is not None:
+#       annual_mean = quarter_mean.groupby(spatial_index).mean()
+#    else:
+    annual_mean = quarter_mean.mean()
+
+    return annual_mean
+
+
+if __name__ == '__main__':
+    annual_mean(2000)
