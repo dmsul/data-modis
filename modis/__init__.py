245a0c1a302f6a7f888a6ecc3317a9d0a4776114
MarkNepf <marknepf@gmail.com>
2018-04-06 11:23:01 -0400

    import 'clean.raw' and 'util.env' functions

---
+++ modis/__init__.py
@@ -0,0 +1,5 @@
+from clean.raw import (load_modis_year, load_modis_day, load_modis_day_hdf,
+                       filenames_modis_day_hdf, hdf_to_df, load_hdf,
+                       print_datasets)
+
+from util.env import data_path
