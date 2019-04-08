fd1f8b59f03ec88b42ce67e531f940aad69722a0
Daniel Sullivan <sullydm@gmail.com>
2019-04-07 20:03:09 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='modis',
-      version='0.0.1',
-      description='Easy access to MODIS data.',
-      url='http://github.com/dmsul/modis',
-      packages=['modis']
-      )
+
+dependencies = [
+]
+
+setup(
+    name='modis',
+    version='0.0.1',
+    description='Easy access to MODIS data.',
+    url='http://github.com/dmsul/modis',
+    author='Daniel M. Sullivan',
+    packages=find_packages(),
+    tests_require=[
+        'pytest',
+    ],
+    package_data={'modis': ["py.typed"]},
+    install_requires=dependencies,
+    zip_safe=False,
+    license='BSD'
+)
